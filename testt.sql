ea25ded4bab706943071b2607ec2518c591fcce7
AgungArdhiyanda <114483578+ardhiyandaa76@users.noreply.github.com>
2022-10-08 14:03:53 +0800

    Tambah table Student

--- testt.sql
+++ testt.sql
@@ -1,3 +1,11 @@
-CREATE DATABASE db_klp4;
+-- CREATE DATABASE db_klp4;
+-- USE db_klp4;
 
-USE db_klp4;
+CREATE table Student(
+	student_name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    major VARCHAR(100) NOT NULL,
+    nim VARCHAR(100) UNIQUE,
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY (id)
+    );
