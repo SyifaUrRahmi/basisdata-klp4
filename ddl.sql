f76eeace47c81aecad6c94a778f391bf708ba9d2
erwinarif31 <muherwinarif31@gmail.com>
2022-10-08 19:47:04 +0800

    rename  to

--- ddl.sql
+++ ddl.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE db_klp4;
 USE db_klp4;
 
-CREATE table Student(
+CREATE table student(
 	id INT UNSIGNED AUTO_INCREMENT,
 	student_name VARCHAR(255) NOT NULL,
    address VARCHAR(255),
@@ -10,12 +10,9 @@ CREATE table Student(
    PRIMARY KEY (id)
 );
 
-DESC course;
-DESC student;
-
 CREATE TABLE course (
 	id INT UNSIGNED AUTO_INCREMENT,
-	couse_name VARCHAR(255),
+	course_name VARCHAR(255),
 	duration INT UNSIGNED,
 	`date` DATE,	
 	`time` TIME,
@@ -23,10 +20,14 @@ CREATE TABLE course (
 );
 
 CREATE TABLE student_schedule (
-   student_id INT AUTO_INCREMENT,
-   course_id INT ,
-   ADD CONSTRAINT student_id FOREIGN KEY (student_id) REFERENCES Student(id) ON UPDATE CASCADE ON DELETE CASCADE ,
-   ADD CONSTRAINT course_id FOREIGN KEY (course_id) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
+   student_id INT UNSIGNED,
+   course_id INT UNSIGNED,
+   FOREIGN KEY (student_id) REFERENCES student(id) ON UPDATE CASCADE ON DELETE CASCADE,
+   FOREIGN KEY (course_id) REFERENCES course(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-SHOW ENGINE INNODB STATUS;
+SHOW TABLES;
+DESC course;
+DESC student;
+DESC student_schedule;
+
