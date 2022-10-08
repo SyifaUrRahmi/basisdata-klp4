c2661a160a7004c8d44110c6c33aa2337dcbb5c9
erwinarif31 <muherwinarif31@gmail.com>
2022-10-08 16:15:16 +0800

    fix table, ada error di create student schedule

--- testt.sql
+++ testt.sql
@@ -3,27 +3,20 @@
 
 CREATE table Student(
 	student_name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    major VARCHAR(100) NOT NULL,
-    nim VARCHAR(100) UNIQUE,
-    id INT AUTO_INCREMENT,
-    PRIMARY KEY (id)
-    );
-
-CREATE TABLE course (
-couse_name VARCHAR(255),
-duration INT UNSIGNED,
-id INT UNSIGNED AUTO_INCREMENT,
-PRIMARY KEY (id)
+	id INT AUTO_INCREMENT,
+   address VARCHAR(255),
+   major VARCHAR(100) NOT NULL,
+   nim VARCHAR(100) UNIQUE,
+   PRIMARY KEY (id)
 );
 
-CREATE TABLE scheduleTable (
-`date` DATE,
-`time` TIME,
-course_id INT,
-id INT UNSIGNED AUTO_INCREMENT,
-PRIMARY KEY(id),
-FOREIGN KEY(course_id) REFERENCES course(id)
+CREATE TABLE course (
+	id INT UNSIGNED AUTO_INCREMENT,
+	couse_name VARCHAR(255),
+	duration INT UNSIGNED,
+	`date` DATE,	
+	`time` TIME,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE student_schedule (
