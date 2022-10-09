3ce5d71a3a040e5eefb46d267c420732004d4afe
erwinarif31 <muherwinarif31@gmail.com>
2022-10-09 11:45:41 +0800

    add api no 6 & 7

--- dml.sql
+++ dml.sql
@@ -6,3 +6,14 @@ values ();
 -- untuk menampilkan data student
 select * from student;
 
+-- Create / input new schedule for specific student
+INSERT INTO course (course_name, duration, `date`, `time`)
+VALUES ("", 1, , ,);
+INSERT INTO student_schedule VALUES (student_id, course_id);
+
+
+-- Edit /update schedule for specific student
+UPDATE course
+set `date` = "yyyy-mm-dd", `time` = hh:mm:ss
+WHERE id = id;
+
