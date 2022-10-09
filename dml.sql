f9f6ea7dfdcf8ea7864ebf62b56a4330181df8f4
erwinarif31 <muherwinarif31@gmail.com>
2022-10-09 11:42:49 +0800

    create a new file for dml

---
+++ dml.sql
@@ -0,0 +1,8 @@
+-- Active: 1665285648409@@127.0.0.1@3306@db_klp4
+--menambahkan data baru pada tabel student
+insert into student	(full_name,address,major)
+values ();
+
+-- untuk menampilkan data student
+select * from student;
+
