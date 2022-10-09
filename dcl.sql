69098cd48c875b1efaa368376988f2e43dba7ca2
AgungArdhiyanda <114483578+ardhiyandaa76@users.noreply.github.com>
2022-10-09 17:10:26 +0800

    Add dcl

---
+++ dcl.sql
@@ -0,0 +1,13 @@
+ -- use db_klp4;
+/* ---------------- DCL -------------------- */
+-- CREATE USER AND STUDENT ACCESS
+ -- CREATE USER 'admin' IDENTIFIED BY 'admin';
+ -- CREATE USER 'student' IDENTIFIED BY 'student123';
+
+-- GRANT ADMIN FOR ALL PRIVILEGES 
+--  GRANT ALL PRIVILEGES ON * TO 'admin';
+
+-- GRANT STUDENT FOR CERTAIN PART OF PRIVILEGES
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON student TO student;
+-- GRANT SELECT, INSERT, UPDATE, DELETE ON student_schedule TO student;
+ 
