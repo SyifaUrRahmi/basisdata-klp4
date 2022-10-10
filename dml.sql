ae31bdda86fa837a5104b05c4b3e1c8a96c23ef7
Muhammad Erwin Arif <87022160+erwinarif31@users.noreply.github.com>
2022-10-10 19:08:52 +0800

    ubah urutan query sesuai tabel API contract

--- dml.sql
+++ dml.sql
@@ -2,11 +2,22 @@
 insert into student	(full_name,address,major)
 values ();
 
--- List of courses
-SELECT id, course_name, duration FROM course;
-
 -- untuk menampilkan data student
 select * from student;
+
+-- update info student
+UPDATE student
+SET student_name ="",address ="", major="" WHERE id = "";
+
+-- get list schedule on specific student
+SELECT c.course_name, c.date, c.time
+FROM student AS s 
+INNER JOIN student_schedule as sc on s.id = sc.student_id
+INNER JOIN course as c on sc.course_id = c.id
+WHERE s.id = 5;
+
+-- List of courses
+SELECT id, course_name, duration FROM course;
 
 -- Create / input new schedule for specific student
 INSERT INTO course (course_name, duration, `date`, `time`)
@@ -19,13 +30,6 @@ UPDATE course
 set `date` = "yyyy-mm-dd", `time` = hh:mm:ss
 WHERE id = id;
 
--- update info student
-UPDATE student
-SET student_name ="",address ="", major="" WHERE id = "";
 
--- get list schedule on specific student
-SELECT c.course_name, c.date, c.time
-FROM student AS s 
-INNER JOIN student_schedule as sc on s.id = sc.student_id
-INNER JOIN course as c on sc.course_id = c.id
-WHERE s.id = 5;
+
+
