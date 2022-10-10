6610a0068f1cbf2dad6a4c42e0c4701198a3a626
AgungArdhiyanda <114483578+ardhiyandaa76@users.noreply.github.com>
2022-10-10 20:19:50 +0800

    fix syntax

--- dcl.sql
+++ dcl.sql
@@ -6,7 +6,7 @@ GRANT ALL PRIVILEGES ON db_klp4.* TO 'developer'@'localhost';
 
 CREATE USER 'student123'@'localhost' IDENTIFIED BY 'student123';
 GRANT SELECT, INSERT, UPDATE, DELETE ON db_klp4.student TO 'student123'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON student_schedule TO 'student123'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON db_klp4.student_schedule TO 'student123'@'localhost';
 
 
  
